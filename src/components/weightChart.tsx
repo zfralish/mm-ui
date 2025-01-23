4ac55e2e9276998b6604b2bd4426455b8670017b
Zeke Fralish <zekefralish@gmail.com>
2025-01-23 17:33:46 -0600

    buncha

--- src/components/weightChart.tsx
+++ src/components/weightChart.tsx
@@ -3,65 +3,48 @@ import {
   AreaChart,
   getFilteredChartTooltipPayload,
   LineChart,
+  ScatterChart,
 } from "@mantine/charts";
 import { Button, Center, Flex, Paper, Stack, Text } from "@mantine/core";
 import { GraphTimelineButtonGroup } from "./graphTimelineButtonGroup";
 import { Weight } from "../api/schema/weights";
 
-interface ChartTooltipProps {
-  label: string;
-  payload: Record<string, any>[] | undefined;
-}
-
-function ChartTooltip({ label, payload }: ChartTooltipProps) {
-  if (!payload || payload.length === 0) return null;
-  const newLabelDate =
-    getFilteredChartTooltipPayload(payload)[0]?.payload?.w_time;
-  const newLabel = format(new Date(newLabelDate), "MMMM d, yyyy h:mm a");
-  return (
-    <Paper px="md" py="sm" withBorder shadow="md" radius="md">
-      <Text fw={500} mb={5}>
-        {newLabel}
-      </Text>
-      {getFilteredChartTooltipPayload(payload).map((item: any) => (
-        <Text key={item.name} c={item.color} fz="sm">
-          {item.name}: {item.value}
-        </Text>
-      ))}
-    </Paper>
-  );
-}
-
 export const data = [
   {
-    date: "Mar 22",
-    Apples: 2890,
-    Oranges: 2338,
-    Tomatoes: 2452,
-  },
-  {
-    date: "Mar 23",
-    Apples: 2756,
-    Oranges: 2103,
-    Tomatoes: 2402,
-  },
-  {
-    date: "Mar 24",
-    Apples: 3322,
-    Oranges: 986,
-    Tomatoes: 1821,
-  },
-  {
-    date: "Mar 25",
-    Apples: 3470,
-    Oranges: 2108,
-    Tomatoes: 2809,
-  },
-  {
-    date: "Mar 26",
-    Apples: 3129,
-    Oranges: 1726,
-    Tomatoes: 2290,
+    color: "blue.5",
+    name: "Group 1",
+    data: [
+      { age: 25, BMI: 20 },
+      { age: 30, BMI: 22 },
+      { age: 35, BMI: 18 },
+      { age: 40, BMI: 25 },
+      { age: 45, BMI: 30 },
+      { age: 28, BMI: 15 },
+      { age: 22, BMI: 12 },
+      { age: 50, BMI: 28 },
+      { age: 32, BMI: 19 },
+      { age: 48, BMI: 31 },
+      { age: 26, BMI: 24 },
+      { age: 38, BMI: 27 },
+      { age: 42, BMI: 29 },
+      { age: 29, BMI: 16 },
+      { age: 34, BMI: 23 },
+      { age: 44, BMI: 33 },
+      { age: 23, BMI: 14 },
+      { age: 37, BMI: 26 },
+      { age: 49, BMI: 34 },
+      { age: 27, BMI: 17 },
+      { age: 41, BMI: 32 },
+      { age: 31, BMI: 21 },
+      { age: 46, BMI: 35 },
+      { age: 24, BMI: 13 },
+      { age: 33, BMI: 22 },
+      { age: 39, BMI: 28 },
+      { age: 47, BMI: 30 },
+      { age: 36, BMI: 25 },
+      { age: 43, BMI: 29 },
+      { age: 21, BMI: 11 },
+    ],
   },
 ];
 
@@ -72,57 +55,15 @@ export default function WeightChart({
   chartData: Weight[];
   wvt: string;
 }) {
-  const formattedChartData = chartData.map((item) => ({
-    ...item,
-    formattedDate: format(new Date(item.w_time), "M/dd"),
-  }));
-
-  // Generate custom ticks aligned to midnight
-  const generateCustomTicks = () => {
-    const sortedDates = chartData
-      .map((item) => new Date(item.w_time))
-      .sort((a, b) => a.getTime() - b.getTime());
-    const startDate = startOfDay(sortedDates[0]); // Align to start of day
-    const endDate = startOfDay(sortedDates[sortedDates.length - 1]); // Align to start of day
-    const daysBetween = differenceInDays(endDate, startDate);
-
-    const tickCount = 12; // You can adjust this number
-    const dayStep = Math.max(1, Math.floor(daysBetween / (tickCount - 1)));
-
-    const customTicks = [];
-    for (let i = 0; i <= daysBetween; i += dayStep) {
-      const tickDate = addDays(startDate, i);
-      customTicks.push(format(tickDate, "M/dd"));
-    }
-
-    return customTicks;
-  };
-
-  const customTicks = generateCustomTicks();
-
   return (
-    <Paper p={10}>
-      <Center>
-        <Stack w={"100%"} h={"100%"} gap={0}>
-          <Flex justify={"space-between"} pl={15} pr={15}>
-            <Text fw={700} size={"lg"} c={"blue"}>
-              Weight vs Time:
-            </Text>
-            <GraphTimelineButtonGroup valKey={"wvt"} />
-          </Flex>
-          <LineChart
-            h={300}
-            data={data}
-            dataKey="date"
-            series={[
-              { name: "Apples", color: "indigo.6" },
-              { name: "Oranges", color: "blue.6" },
-              { name: "Tomatoes", color: "teal.6" },
-            ]}
-            curveType="linear"
-          />
-        </Stack>
-      </Center>
+    <Paper p={15}>
+      <ScatterChart
+        h={250}
+        data={data}
+        dataKey={{ x: "age", y: "BMI" }}
+        xAxisLabel="Age"
+        yAxisLabel="BMI"
+      />
     </Paper>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-
+import "@mantine/charts/styles.css";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
